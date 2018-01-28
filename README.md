5a3a522b2aa531d2484bfcd1ead8cb50a3f7b2a4
Darwin <dhdo@ncsu.edu>
2018-01-28 15:36:43 -0800

    finished part 06, verified reset email

---
+++ README.md
@@ -0,0 +1,12 @@
+ BOOKWORM api
+ 
+- Part 01 <a href="https://www.youtube.com/watch?v=NO2DaxhoWHk">Build Real Web App with React #01</a>
+- Part 02 <a href="https://www.youtube.com/watch?v=RCPMuJ0zYak">Build Real Web App with React #02</a>
+    - Redux setup
+    - Make async request to server and render server validation errors
+- Part 03 <a href="#" />
+- Part 04
+- Part 05
+- Part 06:
+  - created couple of pages and forms and make them smart with thunk actions.
+  - verified JWT on server and how to use expiration JWT feature.
--- src/models/User.js
+++ src/models/User.js
@@ -14,7 +14,8 @@ const schema = new mongoose.Schema(
     unique: true
   },
   passwordHash: {type: String, required: true},
-  confirm: {type: Boolean, default: false}
+  confirmed: {type: Boolean, default: false},
+  confirmationToken: {type: String, default: ""}
   },
   { timesstamps: true})
 
@@ -26,13 +27,36 @@ schema.methods.setPassword = function setPassword(password) {
   this.passwordHash = bcrypt.hashSync(password, 10)
 }
 
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+  this.confirmationToken = this.generateJWT();
+}
+
 schema.methods.generateJWT = function generateJWT() {
   return jwt.sign(
     {
     email: this.email, //public data
+    confirmed: this.confirmed
     },
     process.env.JWT_SECRET);
 }
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+  return `${process.env.HOST}/confirmation/${this.confirmationToken}`
+}
+
+schema.methods.generateResetPasswordLink = function generateResetPasswordLink() {
+  return `${process.env.HOST}/reset_password/${this.generateResetPasswordToken()}`
+}
+
+schema.methods.generateResetPasswordToken = function generateResetPasswordToken() {
+  return jwt.sign(
+    {
+      _id: this._id
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+};
 
 schema.methods.toAuthJSON = function toAuthJSON() {
   return {
